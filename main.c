b6c2498d51a32783c14821441db7f3a96da4829d
dschaly2 <drewschaly@gmail.com>
2022-12-09 21:12:01 -0500

    Shell running, buffers set up, and exit command working.

--- main.c
+++ main.c
@@ -1,6 +1,59 @@
 // Author: Drew Schaly
 // 12/9/2022
 
-int main(int argc, char* argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+
+typedef struct {
+	char* buffer;
+	size_t buffLength;
+	ssize_t inputLength;
+} InputBuffer;
+
+InputBuffer* newInputBuffer() {
+	InputBuffer* inputBuffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+	inputBuffer->buffer = NULL;
+	inputBuffer->buffLength = 0;
+	inputBuffer->inputLength = 0;
+
+	return inputBuffer;
+}
+
+void printPrompt() { printf("(.) > "); }
+
+void readInput(InputBuffer* inputBuffer) {
+	ssize_t bytesRead = getline(&(inputBuffer->buffer), &(inputBuffer->buffLength), stdin);
+
+	if (bytesRead <= 0) {
+		printf("Failed to read input! :(\n");
+		exit(EXIT_FAILURE);
+	}
+
+	inputBuffer->inputLength = bytesRead - 1;
+	inputBuffer->buffer[bytesRead - 1] = 0;
+}
+
+void closeInputBuffer(InputBuffer* inputBuffer) {
+	free(inputBuffer->buffer);
+	free(inputBuffer);
+}
+
+
+int main(int argc, char* argv[]) {
+	InputBuffer* inputBuffer = newInputBuffer();
+
+	while(1){
+		printPrompt();
+
+		readInput(inputBuffer);
+
+		if (strcmp(inputBuffer->buffer, "exit") == 0) {
+			closeInputBuffer(inputBuffer);
+			exit(EXIT_SUCCESS);
+		} else {
+			printf("IDK that command bro: '%s' \n", inputBuffer->buffer);
+		}
+	}
 }
